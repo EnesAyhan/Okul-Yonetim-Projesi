7f087de98579f65123482758c6398c1e288b2295
Enes AYHAN <enesayhan3232@gmail.com>
2022-06-17 18:07:54 +0300

    guncelleme

--- src/OkulYonetimPaneli/Runner.java
+++ src/OkulYonetimPaneli/Runner.java
@@ -5,6 +5,6 @@ import static OkulYonetimPaneli.Islemler.girisPaneli;
 public class Runner {
     public static void main(String[] args) {
         girisPaneli();//static method import edildi
-        System.out.println("islem tamamlandi");
+
     }
 }
